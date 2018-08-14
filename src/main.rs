0db422d68eeb5a1efd948302c430851b9dcd9134
William Wold <wm@wmww.sh>
2018-08-14 08:23:22 -0700

    added command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+mod options;
+
+pub enum Priority {
+    Debug,
+    InternalWarning,
+    InternalError,
+    Warning,
+    Error,
+}
+
+pub fn message(priority: Priority, msg: &str) {
+    println!(
+        "{}: {}",
+        match priority {
+            Priority::Debug => "Debug",
+            Priority::InternalWarning => "Internal warning",
+            Priority::InternalError => "Internal error",
+            Priority::Warning => "Warning",
+            Priority::Error => "Error",
+        },
+        msg
+    );
+}
+
+pub fn debug(msg: &str) {
+    message(Priority::Debug, msg);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let options = options::Options::new_default().with_cmd_line();
+    if let Some(s) = options.filepath {
+        debug(&format!("Loading source code: {}", s));
+    }
 }
---
+++ src/options.rs
@@ -0,0 +1,61 @@
+use std;
+
+extern crate clap;
+use self::clap::{App, Arg};
+
+pub struct Options {
+    pub filepath: Option<String>, // code to run
+    pub fixup_file: bool,         // if to automatically fix problems found in the file
+    pub debug: bool,              // if to run bft in debug mode
+}
+
+impl Options {
+    pub fn new_default() -> Options {
+        Options {
+            filepath: None,
+            fixup_file: true,
+            debug: false,
+        }
+    }
+
+    pub fn with_cmd_line(self) -> Options {
+        let mut app = App::new(env!("CARGO_PKG_NAME"))
+            .version(env!("CARGO_PKG_VERSION"))
+            .author(env!("CARGO_PKG_AUTHORS"))
+            .about(env!("CARGO_PKG_DESCRIPTION"))
+            .arg(
+                Arg::with_name("FILEPATH")
+                    .help("The input brainfuck source code")
+                    .index(1),
+            )
+            .arg(
+                Arg::with_name("DEBUG")
+                    .short("d")
+                    .long("debug")
+                    .help("Enable debug mode"),
+            )
+            .arg(
+                Arg::with_name("READONLY")
+                    .short("r")
+                    .long("readonly")
+                    .help("Don't apply fixes to source files if problems are found"),
+            );
+        let matches = app.clone().get_matches();
+        let mut options = self;
+        options.filepath = match matches.value_of("FILEPATH") {
+            Some(s) => Some(s.to_string()),
+            None => None,
+        };
+        if matches.is_present("DEBUG") {
+            options.debug = true;
+        }
+        if matches.is_present("READONLY") {
+            options.fixup_file = false;
+        }
+        if options.filepath == None {
+            app.write_long_help(&mut std::io::stdout())
+                .expect("failed to write to stdout");
+        }
+        return options;
+    }
+}
